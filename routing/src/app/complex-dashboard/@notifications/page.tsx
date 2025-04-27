e57c52be0daa554ca1db3d9e7d07e2b74a072d8a
sreehithakambhampati <kambhampatisreehitha@gmail.com>
2025-04-27 13:03:20 +0530

    Parallel routes

---
+++ routing/src/app/complex-dashboard/@notifications/page.tsx
@@ -0,0 +1,9 @@
+import { Card } from "@/components/card";
+
+export default async function NotificationMetrics() {
+//   await new Promise((resolve) => setTimeout(resolve, 20000)); // Wait for 2 ms
+
+  return (
+    <Card>Notification Metrics</Card>
+  );
+}
---
+++ routing/src/app/complex-dashboard/page.tsx
@@ -0,0 +1,5 @@
+export default function DashboardPage(){
+    return(
+        <h1>Complex Dashboard</h1>
+    )
+}
