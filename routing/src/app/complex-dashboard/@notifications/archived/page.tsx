fab02246333254608f6f063406dc5ca4caae5a57
sreehithakambhampati <kambhampatisreehitha@gmail.com>
2025-04-27 15:50:34 +0530

    Sub navigation in routes

--- routing/src/app/complex-dashboard/@notifications/archived/page.tsx
+++ routing/src/app/complex-dashboard/@notifications/archived/page.tsx
@@ -1,14 +1,15 @@
 import { Card } from "@/components/card";
 import Link from "next/link";
 
-export default async function NotificationMetrics() {
+export default async function ArchivedNotifications() {
 //   await new Promise((resolve) => setTimeout(resolve, 20000)); // Wait for 2 ms
 
   return (
     <>
-    <Card>Notification Metrics</Card>
-    {/* Sub-navigation in routes */}
-    <Link href="complex-dashboard/archived">Archived</Link>
+    <Card>Archived Notifications
+    <Link href="/complex-dashboard">Default</Link>
+    </Card>
+    
     </>
   );
 }
