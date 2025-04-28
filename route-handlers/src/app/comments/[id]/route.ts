2e96d8165f634c202bec04b3206651297375b7cc
sreehithakambhampati <kambhampatisreehitha@gmail.com>
2025-04-28 16:21:47 +0530

    Delete and patch

--- route-handlers/src/app/comments/[id]/route.ts
+++ route-handlers/src/app/comments/[id]/route.ts
@@ -5,3 +5,23 @@ export async function GET(request:Request,{params}:{params:Promise<{id:string}>}
     const comment = comments.find((comment) => comment.id === parseInt(id))
     return Response.json(comment)
 }
+
+export async function PATCH(request:Request,{params}:{params:Promise<{id:string}>})
+{
+    const {id} = await params
+    const body = await request.json()
+    const {text} = body
+    const index = parseInt(id)-1;
+    // const index = comments.findIndex((comment)=>comment.id===parseInt(id))
+    comments[index].text = text;
+    return Response.json(comments[index])
+}
+
+export async function DELETE(request:Request,{params}:{params:Promise<{id:string}>}){
+    const {id} = await params
+    const index = comments.findIndex((comment)=>comment.id===parseInt(id))
+    const deletedComment = comments[index]
+    // array.splice(startIndex, deleteCount)
+    comments.splice(index,1)
+    return Response.json(deletedComment)
+}
