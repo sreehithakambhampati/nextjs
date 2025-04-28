d59a827ff49a22c426a183c0b66f28358f1615ba
sreehithakambhampati <kambhampatisreehitha@gmail.com>
2025-04-28 16:54:51 +0530

    Headers in Route Handlers

--- route-handlers/src/app/comments/route.ts
+++ route-handlers/src/app/comments/route.ts
@@ -1,8 +1,14 @@
 
+import { NextRequest } from "next/server";
 import {comments} from "./comment"
-export async function GET(){
+//Query parameter handling
+export async function GET(request : NextRequest){
     // Response.json() is a method used to create a Response object that contains JSON data in the body. It is part of the Fetch API and is commonly used in web servers and APIs to return JSON data in response to HTTP requests.
-     return Response.json(comments)
+    const searchParams = request.nextUrl.searchParams;
+    const query = searchParams.get("query")
+    const filteredComments = query ? comments.filter((comment)=>comment.text.includes(query)) : comments
+
+     return Response.json(filteredComments)
 }
 export async function POST(request:Request){
      const comment = await request.json()
--- route-handlers/src/app/profile/api/route.ts
+++ route-handlers/src/app/profile/api/route.ts
@@ -1,3 +1,21 @@
-export async function GET() {
-    return new Response("User profile");
+import { NextRequest } from "next/server";
+import { headers } from "next/headers";
+
+export async function GET(request : NextRequest) {
+    // const headers =new Headers(request.headers);
+    // const token = headers.get("Authorization")
+    const headersList = await headers()
+    const token = headersList.get("Authorization")
+    console.log(token)
+    
+    // If we use return new Response("<h1>User profile</h1>"); in /profile/api, it will simply display the raw HTML as plain text.
+    // return new Response("<h1>User profile</h1>");
+    
+    // However, if we write return new Response("<h1>User profile</h1>", { headers: { "Content-Type": "text/html" } });, the server informs the browser that the response is HTML content.
+// As a result, the text is properly rendered as an actual heading (<h1>) on the /profile/api page.
+    return new Response("<h1>User profile</h1>",{headers:{"Content-Type" : "text/html"}});
+    
+    
+
+
   }
