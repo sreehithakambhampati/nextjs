e0bd27aecf9ca8ef4a6ef812e26abbd63a45fca6
sreehithakambhampati <kambhampatisreehitha@gmail.com>
2025-04-29 10:47:33 +0530

    Cookies in route handlers

--- route-handlers/src/app/comments/route.ts
+++ route-handlers/src/app/comments/route.ts
@@ -6,7 +6,7 @@ export async function GET(request : NextRequest){
     // Response.json() is a method used to create a Response object that contains JSON data in the body. It is part of the Fetch API and is commonly used in web servers and APIs to return JSON data in response to HTTP requests.
     const searchParams = request.nextUrl.searchParams;
     const query = searchParams.get("query")
-    const filteredComments = query ? comments.filter((comment)=>comment.text.includes(query)) : comments
+    const filteredComments = query ? comments.filter((comment)=>comment.text.includes(query)) : comments    
 
      return Response.json(filteredComments)
 }
--- route-handlers/src/app/profile/api/route.ts
+++ route-handlers/src/app/profile/api/route.ts
@@ -1,5 +1,5 @@
 import { NextRequest } from "next/server";
-import { headers } from "next/headers";
+import { headers,cookies } from "next/headers";
 
 export async function GET(request : NextRequest) {
     // const headers =new Headers(request.headers);
@@ -7,13 +7,26 @@ export async function GET(request : NextRequest) {
     const headersList = await headers()
     const token = headersList.get("Authorization")
     console.log(token)
+
+
+    const theme = request.cookies.get("theme")
+    console.log(theme)
+    const cookiesList = await cookies()
+    cookiesList.set("resultsperPage","20")
+     const theme1 = cookiesList.get("resultsperPage")
+     console.log(theme1)
+
+
     
     // If we use return new Response("<h1>User profile</h1>"); in /profile/api, it will simply display the raw HTML as plain text.
     // return new Response("<h1>User profile</h1>");
     
     // However, if we write return new Response("<h1>User profile</h1>", { headers: { "Content-Type": "text/html" } });, the server informs the browser that the response is HTML content.
-// As a result, the text is properly rendered as an actual heading (<h1>) on the /profile/api page.
-    return new Response("<h1>User profile</h1>",{headers:{"Content-Type" : "text/html"}});
+   // As a result, the text is properly rendered as an actual heading (<h1>) on the /profile/api page.
+    return new Response("<h1>User profile</h1>",
+        {headers:{"Content-Type" : "text/html",
+            "Set-Cookie" : "theme=dark",
+        }});
     
     
 
