3647425cc89029808deb3317dce6baf73983e265
sreehithakambhampati <kambhampatisreehitha@gmail.com>
2025-05-16 19:31:01 +0530

    Context Provider

--- rendering/src/app/client-route/page.tsx
+++ rendering/src/app/client-route/page.tsx
@@ -3,12 +3,17 @@ import React from 'react';
 import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
+// import { useTheme } from '@/components/ThemeProvider';
+import { useContext } from 'react';
+import {ThemeContext} from '@/components/ThemeProvider';
 
 
 export default function ClientRoutePage() {
   const settings = {
     dots: true,
   };
+  const theme = useContext(ThemeContext)
+  
   return (
     <div className="image-slider-container">
       <Slider {...settings}>
@@ -25,6 +30,9 @@ export default function ClientRoutePage() {
           <img src="http://picsum.photos/400/200" />
         </div>
       </Slider>
+      <div style={{ color: theme.colors.primary }}>
+      <h1>Client route page</h1>
+    </div>
     </div>
   );
 }
