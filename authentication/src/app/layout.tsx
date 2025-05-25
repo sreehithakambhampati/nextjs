236908e160da68397b9322bb2177152f412fd332
sreehithakambhampati <kambhampatisreehitha@gmail.com>
2025-05-25 15:04:40 +0530

    SignIn and SignOut

--- authentication/src/app/layout.tsx
+++ authentication/src/app/layout.tsx
@@ -2,6 +2,7 @@ import { ClerkProvider } from "@clerk/nextjs";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { Navigation } from "@/components/navigation";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -29,6 +30,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <Navigation/>
         {children}
       </body>
     </html>
